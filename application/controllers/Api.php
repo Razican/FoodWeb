ff7c1afda40697a9a75b6cb27e410c5157740af6
Razican <admin@razican.com>
2014-09-11 16:00:18 +0300

    Added first API files

---
+++ application/controllers/Api.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	public function index()
+	{
+		$data['response'] = "This is the API test";
+		$this->load->view('api', $data);
+		// TODO
+	}
+
+	public function register()
+	{
+		// TODO
+	}
+
+	public function login()
+	{
+		// TODO
+	}
+
+	public function search()
+	{
+		// TODO
+	}
+
+	public function reset_pass()
+	{
+		// TODO
+	}
+}
+
+/* End of file Api.php */
+/* Location: ./application/controllers/Api.php */
